2cd29bc39e7dd12011a3df2e652fb230d0044df7
vahxjk <149882968+vahxjk@users.noreply.github.com>
2024-11-06 06:29:49 +0700

    Add files via upload
    complete entity

---
+++ AppChat/src/main/java/com/example/appchat/entity/Contact.java
@@ -0,0 +1,47 @@
+package com.example.appchat.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.web.JsonPath;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @Column(name = "contact_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "username",nullable = false)
+    String username;
+
+    @Column(name = "phone",nullable = false)
+    String phone;
+
+    @Column(name = "email",nullable = false)
+    String email;
+
+    @Column(name = "created_at",updatable = false)
+    @CreationTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    User user;
+}
---
+++ AppChat/src/main/java/com/example/appchat/entity/Image.java
@@ -0,0 +1,36 @@
+package com.example.appchat.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id",nullable = false)
+    Integer id;
+
+    @Column(name = "image_url",nullable = false)
+    String url;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @CreationTimestamp
+    @Column(name = "created_at",updatable = false)
+    LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "message_id", columnDefinition = "message_id")
+    Image image;
+}
